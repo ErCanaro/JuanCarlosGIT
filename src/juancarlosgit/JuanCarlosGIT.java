106bbcf3cba8ec9bb8614512ea7be36f95f9a863
Enzo Bulacio <72264564+ErCanaro@users.noreply.github.com>
2023-08-08 18:42:37 -0300

    Segundo Commit
    Se agregó un uevo sout para el segundo commit!

--- src/juancarlosgit/JuanCarlosGIT.java
+++ src/juancarlosgit/JuanCarlosGIT.java
@@ -7,6 +7,8 @@ public class JuanCarlosGIT {
 
     public static void main(String[] args) {
         System.out.println("Esto es una prueba de GIT");
+        
+        System.out.println("Esto es para el segundo COMMIT");
     }
     
 }
