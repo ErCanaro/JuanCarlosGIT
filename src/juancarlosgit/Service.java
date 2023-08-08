f9ecf9b13c95936a52369c2f871053d6b4cc3f76
Enzo Bulacio <72264564+ErCanaro@users.noreply.github.com>
2023-08-08 19:29:32 -0300

    Branch Commit
    Probando un Merge desde Branch a Main

--- src/juancarlosgit/Service.java
+++ src/juancarlosgit/Service.java
@@ -5,6 +5,9 @@ package juancarlosgit;
 public class Service {
     
     public static int suma(int a, int b){
+        
+        System.out.println("Probando MERGE TOMPSON");
+        
         return a+b; //Que bruto póngale cero.
     }
     
